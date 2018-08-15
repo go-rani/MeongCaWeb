0e45b6ee27f8c1300b6f52dfd4901b1855b2853f
윤수연 <shsiss007@gmail.com>
2018-08-15 18:39:45 +0900

    소스트리로 커밋하기

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ class App extends Component {
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <p className="App-intro">
+          소스트리로 커밋해보기
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
       </div>
